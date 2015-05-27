f87b16f3626678c5a90e3047c31b6120feb76113
Art <artur.adimax@gmail.com>
2015-05-27 13:47:49 +0300

    Update save_form.php

--- save_form.php
+++ save_form.php
@@ -4,7 +4,7 @@
  </head>
  <body>
 <?php
-$query1 = "INSERT INTO registration(name, pass) values('".$_POST['name']."','".$_POST["pass"]."')"
+$query1 = "INSERT INTO registration(name, pass) values('".$_POST['name']."','".$_POST["pass"]."')";
 
 mysql_query($query1) or die(mysql_error());
 ?>
