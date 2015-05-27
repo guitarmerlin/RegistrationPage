a9091f9f429bab6852d1d358167fee41e151d47c
Art <artur.adimax@gmail.com>
2015-05-27 13:32:32 +0300

    Create save_form.php

---
+++ save_form.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>test PHP</title>
+ </head>
+ <body>
+<?php
+$query = "INSERT INTO registration(name, pass) values('".$_POST['name']."','".$_POST["pass"]."')"
+?>
+ </body>
+</html>
