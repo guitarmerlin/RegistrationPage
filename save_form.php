6f812658962f40947863929b80f98cd1eae3b561
Art <artur.adimax@gmail.com>
2015-05-27 16:01:50 +0300

    Update save_form.php

--- save_form.php
+++ save_form.php
@@ -4,7 +4,37 @@
  </head>
  <body>
 <?php
-$query1 = "INSERT INTO registration(name, pass) values('".$_POST['name']."','".$_POST["pass"]."')";
+
+$link = mysql_connect( 'localhost' , 'guitarmerlin', '80177413')
+    or die('cant connect ' . mysql_error());
+echo 'data from table: ';
+mysql_select_db('guitarmerlin') or die('i cant use BD');
+
+
+
+// Выполняем SQL-запрос
+$query = 'SELECT * FROM registration';
+$result = mysql_query($query) or die('donw work ' . mysql_error());
+
+// Выводим результаты в html
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+
+ 
+/* Выполняем запрос. Если произойдет ошибка - вывести ее. */
+mysql_query($query) or die(mysql_error());
+
+
+
+$query1 = "INSERT INTO registration(RegistrationName, RegistrationPass) values('".$_POST['name']."','".$_POST["pass"]."')";
 
 mysql_query($query1) or die(mysql_error());
 ?>
